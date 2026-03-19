d6434e224d4f70c3b193226b533f16b96680b020
AndiCoding <andi98@live.no>
2026-03-19 10:21:42 +0100

    updates to kubernetes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/features/hero/Hero";
 import { Projects } from "@/features/projects/Projects";
-import { getStaticProjects }                                     from "./actions/projectActions";
+import { getStaticProjects } from "./actions/projectActions";
 import {Suspense} from "react";
 import LenisWrapper from "@/features/lenisWrapper/LenisWrapper";
 
+export const revalidate = 20;
 
 export default async function Home() {
     const projects = await getStaticProjects();
@@ -15,7 +16,7 @@ export default async function Home() {
               {/*<Experience id="experience" className="w-full " />*/}
               <div className="h-full"></div>
               <Suspense fallback={<div>... Loading</div>}>
-                  <Projects projects={projects.props.projects} className=" w-full drop-shadow-2xl z-100 " />
+                  <Projects projects={projects} className=" w-full drop-shadow-2xl z-100 " />
               </Suspense>
           </LenisWrapper>
         
